5294a32461d7f5251da47d6495da21040318eca3
kingofday <shahrooz.bazrafshan@gmail.com>
2020-05-21 22:37:42 +0430

    main slideer

---
+++ InsBrokers.Portal/wwwroot/Scripts/App/Home/index.js
@@ -0,0 +1,9 @@
+﻿///<reference path="../../Libs/jquery-3.1.1.min.js" />
+$(document).ready(function () {
+    $('#main-slider').slick({
+        rtl: true,
+        autoplay: true,
+        adaptiveHeight: true,
+        slidesToShow: 1
+    });
+});
