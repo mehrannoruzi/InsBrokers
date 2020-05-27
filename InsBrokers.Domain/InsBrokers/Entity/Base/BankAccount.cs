641be5f91aade58d699ab013aadcf1493fb20486
kingofday <shahrooz.bazrafshan@gmail.com>
2020-05-27 17:58:53 +0430

    change shaba length

--- InsBrokers.Domain/InsBrokers/Entity/Base/BankAccount.cs
+++ InsBrokers.Domain/InsBrokers/Entity/Base/BankAccount.cs
@@ -51,9 +51,10 @@ namespace InsBrokers.Domain
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "varchar(24)")]
+        [RegularExpression(@"IR\d()")]
         [Display(Name = nameof(Strings.Shaba), ResourceType = typeof(Strings))]
-        [MaxLength(24, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
-        [StringLength(24, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [MaxLength(26, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [StringLength(26, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
         public string Shaba { get; set; }
     }
 }
