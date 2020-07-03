05a22ffbb18a5ad04290171b4d96781abfef5ff7
Mehran Norouzi <mehrannoruzi@gmail.com>
2020-07-03 23:25:59 +0430

    Add ContactUs Entity

---
+++ InsBrokers.DataAccess.Ef/Migrations/AppDb/20200703185409_AddContactUs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InsBrokers.DataAccess.Ef.Migrations.AppDb
+{
+    public partial class AddContactUs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactUs",
+                schema: "Base",
+                columns: table => new
+                {
+                    ContactUsId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<Guid>(nullable: true),
+                    MobileNumber = table.Column<long>(nullable: false),
+                    InsertDateMi = table.Column<DateTime>(nullable: false),
+                    InsertDateSh = table.Column<string>(type: "char(10)", maxLength: 10, nullable: true),
+                    FullName = table.Column<string>(maxLength: 50, nullable: false),
+                    Subject = table.Column<string>(maxLength: 50, nullable: false),
+                    Content = table.Column<string>(maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactUs", x => x.ContactUsId);
+                    table.ForeignKey(
+                        name: "FK_ContactUs_User_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Base",
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactUs_UserId",
+                schema: "Base",
+                table: "ContactUs",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactUs",
+                schema: "Base");
+        }
+    }
+}
---
+++ InsBrokers.Domain/InsBrokers/Entity/Base/ContactUs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Elk.Core;
+using InsBrokers.Domain.Resource;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsBrokers.Domain
+{
+    [Table(nameof(ContactUs), Schema = "Base")]
+   public class ContactUs : IInsertDateProperties, IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ContactUsId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [Display(Name = nameof(Strings.User), ResourceType = typeof(Strings))]
+        public User User { get; set; }
+        [Display(Name = nameof(Strings.User), ResourceType = typeof(Strings))]
+        public Guid? UserId { get; set; }
+
+        [Display(Name = nameof(Strings.MobileNumber), ResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = nameof(ErrorMessage.Required), ErrorMessageResourceType = typeof(ErrorMessage))]
+        public long MobileNumber { get; set; }
+
+        [Display(Name = nameof(Strings.InsertDate), ResourceType = typeof(Strings))]
+        public DateTime InsertDateMi { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        [Display(Name = nameof(Strings.InsertDate), ResourceType = typeof(Strings))]
+        [MaxLength(10, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        public string InsertDateSh { get; set; }
+
+        [Display(Name = nameof(Strings.FullName), ResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = nameof(ErrorMessage.Required), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [MaxLength(50, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [StringLength(50, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        public string FullName { get; set; }
+
+        [Display(Name = nameof(Strings.Subject), ResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = nameof(ErrorMessage.Required), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [MaxLength(50, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [StringLength(50, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        public string Subject { get; set; }
+
+        [Display(Name = nameof(Strings.Content), ResourceType = typeof(Strings))]
+        [Required(ErrorMessageResourceName = nameof(ErrorMessage.Required), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [MaxLength(500, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        [StringLength(500, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
+        public string Content { get; set; }
+    }
+}
